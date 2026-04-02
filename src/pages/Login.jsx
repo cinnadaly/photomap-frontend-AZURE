843325d3358a034d8fed85023b2456fc037b6b52
cinnadaly <nadalygalindo@gmail.com>
2026-04-01 19:22:51 -0700

    login fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,11 +2,11 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 import "../components/HomeMap.css";
 
-function Login({setIsAuthenticated, setUser}){
+function Login({ setIsAuthenticated, setUser }) {
 
   const navigate = useNavigate();
 
-// The action function can be async
+  // The action function can be async
   /*const getAll = async () => {
     const response = await fetch('http://localhost:5555/users', {
       method: 'GET',
@@ -22,20 +22,20 @@ function Login({setIsAuthenticated, setUser}){
   };*/
 
 
-// The action function can be async
+  // The action function can be async
   const myAction = async (formData) => {
     const data = Object.fromEntries(formData);
     console.log(data)
     // Server-side logic or client-side logic in a Transition
-    const response = await fetch('http://localhost:5555/login', {
-      method: 'POST', 
+    const response = await fetch('https://photomap-e0h6fnh3hxfscbc8.westus3-01.azurewebsites.net/login', {
+      method: 'POST',
       credentials: 'include',
       headers: {
         'Content-Type': 'application/json', // 2. Tell the server to expect JSON
       },
       // 3. Stringify the object for the body
       body: JSON.stringify(data),
-      
+
     })
 
     if (response.ok) {
@@ -56,7 +56,7 @@ function Login({setIsAuthenticated, setUser}){
   return (
     // When using a function for 'action', method defaults to POST
     <>
-    <div className='home-component '>
+      <div className='home-component '>
         <div className="container-fluid">
           <div className="row profile-row d-flex justify-content-center align-items-center">
             <div className="col-12 col-lg-5 ">
@@ -65,21 +65,21 @@ function Login({setIsAuthenticated, setUser}){
                 const formData = new FormData(e.target);
                 await myAction(formData);
               }}>
-                  <fieldset >
-                    <legend>Login</legend>
-                      <div class="mb-3">
-                        <input type="text" name="username" id="disabledTextInput" class="form-control" placeholder="username"/>
-                      </div>
-                      <div class="mb-3">
-                        <input type="text" name="password" id="disabledTextInput" class="form-control" placeholder="password"/>
-                      </div>
-                        <button type="submit" class="btn btn-primary">Login</button>
-                  </fieldset>
+                <fieldset >
+                  <legend>Login</legend>
+                  <div class="mb-3">
+                    <input type="text" name="username" id="disabledTextInput" class="form-control" placeholder="username" />
+                  </div>
+                  <div class="mb-3">
+                    <input type="text" name="password" id="disabledTextInput" class="form-control" placeholder="password" />
+                  </div>
+                  <button type="submit" class="btn btn-primary">Login</button>
+                </fieldset>
               </form>
             </div>
           </div>
         </div>
-    </div>
+      </div>
 
     </>
   );
@@ -95,6 +95,6 @@ function Login({setIsAuthenticated, setUser}){
    * 
    */
 
-  }
+}
 
 export default Login
