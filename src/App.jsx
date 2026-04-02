fbaa3eec7a38f3aea75ff580f5c3e3e3ef429b45
cinnadaly <nadalygalindo@gmail.com>
2026-04-01 21:44:44 -0700

    fx: vite test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
