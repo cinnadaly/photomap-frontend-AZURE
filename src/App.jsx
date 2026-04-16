c5b5a0f102de1b12cf09f6dfdaa446f2ec70e8f5
Alejandro Vaquera Lopez <alejandrovaqueralopez@gmail.com>
2026-04-15 19:13:32 -0700

    env

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ function App() {
       credentials: 'include',
     })
       .then((response) => {
-        console.log(response.message)
+        console.log(response)
         if (response.ok) {
           setIsAuthenticated(false);
           //navigate("/login");
