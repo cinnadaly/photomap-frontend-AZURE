e4185d51fc9fc7f440d787a7d79adbe9e06b9425
Alejandro Vaquera Lopez <alejandrovaqueralopez@gmail.com>
2026-04-21 02:33:23 -0700

    feat:

--- src/components/DashboardPhoto.jsx
+++ src/components/DashboardPhoto.jsx
@@ -24,6 +24,28 @@ function DashboardPhoto({ photo, onDelete }) {
                     className="btn-edit-custom mt-2 bg-none">
                     <i class="bi bi-pen-fill"></i>
                   </button>
+
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
                   <button
                     onClick={() => onDelete(photo.id)}
                     className="btn-absolute-custom mt-2 bg-none">
