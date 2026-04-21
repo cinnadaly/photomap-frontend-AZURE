f8a6d05588bc868b47c600515cf556d896df7a06
Alejandro Vaquera Lopez <alejandrovaqueralopez@gmail.com>
2026-04-21 01:07:40 -0700

    feat:

--- src/components/DashboardPhoto.jsx
+++ src/components/DashboardPhoto.jsx
@@ -44,7 +44,7 @@ function DashboardPhoto({ photo, onDelete }) {
                   <div class="accordion-body">
                     <ul class="list-group list-group-flush">
                       {
-                        console.log(photo.review)/*
+                        console.log(photo.reviews)/*
                         photo.reviews.map((review)=> {
                           <li class="list-group-item">
                             <h5>Comment: {review.Comment}</h5>
