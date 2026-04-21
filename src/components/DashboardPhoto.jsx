e335662291c7ce77e5bde6cfd9f43f22e43d53a8
Alejandro Vaquera Lopez <alejandrovaqueralopez@gmail.com>
2026-04-21 02:45:18 -0700

    feat:

--- src/components/DashboardPhoto.jsx
+++ src/components/DashboardPhoto.jsx
@@ -4,6 +4,29 @@ import '../components/HomeMap.css';
 
 function DashboardPhoto({ photo, onDelete }) {
 
+
+  const handleSubmit = async (e) => {
+      e.preventDefault();
+      const formData = new FormData();
+      formData.append("photo", file);
+      formData.append("userID", userID);
+      formData.append("title", title);
+      formData.append("description", description);
+      try {
+          const response = await fetch("https://photomap-e0h6fnh3hxfscbc8.westus3-01.azurewebsites.net/reviews", {
+              method: "POST",
+              credentials: "include",
+              body: formData,
+          });
+
+          const data = await response.json();
+          console.log(data);
+
+      } catch (error) {
+          console.error("Error:", error);
+      }
+  };
+
   const currentUser = JSON.parse(localStorage.getItem("user"));
     return (
         <figure className="figure py-2 mx-2">
@@ -19,33 +42,40 @@ function DashboardPhoto({ photo, onDelete }) {
 
               {currentUser && photo.userID === currentUser.id && (
                 <div className="position-absolute">
-                  <button
-                    onClick={() => onEdit(photo.id)}
-                    className="btn-edit-custom mt-2 bg-none">
-                    <i class="bi bi-pen-fill"></i>
+
+                  <button onClick={() => onEdit(photo.id)} type="button" className="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Add comment
                   </button>
 
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
+                  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h1 class="modal-title fs-5" id="exampleModalLabel">New Comment</h1>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
 
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+                        <form onSubmit={handleSubmit}>
+                            <div className="row d-flex justify-content-center">
+                                <div className="col-12 col-lg-5 ">
+                                    <input className='upload-form-container txt-file-input' type="file" onChange={(e) => setFile(e.target.files[0])} required />
+                                </div>
+                                <div className="col-12 col-lg-5 ">
+                                    <div className="upload-form-container d-flex flex-column p-5 ">
+                                        <input className='form-control from-control-lg my-1' type="text" placeholder="Título" value={title} onChange={(e) => setTitle(e.target.value)} />
+                                        <input className='form-control from-control-lg my-1' type="text" placeholder="Descripción" value={description} onChange={(e) => setDescription(e.target.value)} />
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="submit" class="btn btn-primary">Save changes</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+
+                      </div>
+                    </div>
+                  </div>
+
+
                   <button
                     onClick={() => onDelete(photo.id)}
                     className="btn-absolute-custom mt-2 bg-none">
