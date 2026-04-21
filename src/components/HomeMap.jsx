62b52e4ce75995140af279850146fdf76c32e801
Alejandro Vaquera Lopez <alejandrovaqueralopez@gmail.com>
2026-04-20 20:48:03 -0700

    selected location

--- src/components/HomeMap.jsx
+++ src/components/HomeMap.jsx
@@ -206,6 +206,7 @@ export default function HomeMap(){
                                                 }
                                                 return rows;*/
                                                 selectedLocationReview
+
                                             })()
                                         }
                                     </div>
