e58594ba7502ad78601e0c10dd26bc902253292e
Alejandro Vaquera Lopez <alejandrovaqueralopez@gmail.com>
2026-04-21 03:03:01 -0700

    feat:

--- src/components/DashboardPhoto.jsx
+++ src/components/DashboardPhoto.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomeMap from '../components/HomeMap';
 import '../components/HomeMap.css';
-import useState from "React";
+import {useState, useEffect} from "React";
 
 
 function DashboardPhoto({ photo, onDelete }) {
