dfcbac87dfc13301cf704a9395fcec594619fb00
Alejandro Vaquera Lopez <alejandrovaqueralopez@gmail.com>
2026-04-20 22:13:59 -0700

    selected location

--- src/components/HomeMap.jsx
+++ src/components/HomeMap.jsx
@@ -215,7 +215,7 @@ export default function HomeMap(){
                                             })()
 
                                         }
-                                        { <h3><> </>selectedLocationReview.location.avgRating</h3>}
+                                        { <h6><> </>{selectedLocationReview.location.avgRating}</h6>}
                                     </div>
                                     <p><strong>Lat:</strong> {selectedLocation.lat}</p>
                                     <p><strong>Lng:</strong> {selectedLocation.lng}</p>
