460ba27d4b4bb671b443914b88e167383ab43ca9
cinnadaly <nadalygalindo@gmail.com>
2026-04-20 22:30:29 -0700

    ft: user dashboard

--- src/components/DashboardPhoto.jsx
+++ src/components/DashboardPhoto.jsx
@@ -2,25 +2,32 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomeMap from '../components/HomeMap';
 import '../components/HomeMap.css';
 
-function DashboardPhoto({photo, onDelete}){
+function DashboardPhoto({ photo, onDelete }) {
 
-    return (
-        <figure className="figure py-2 mx-2 rounded display-flex">
-          <div className="figure-img img-fluid rounded" alt="..." 
-            style={{
-              backgroundImage: `url(${photo.imagePath})`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              height: "250px",
-              width: "100%"
-            }}></div>
-          <div>
-            <figcaption className="figure-caption">{photo.title}</figcaption>
-            <p className="figure-caption">{photo.description}</p>
-            <button onClick={() => onDelete(photo.id)} className="btn btn-dark mt-2">Delete</button>
-          </div>
-        </figure>
-    );
-  }
+  const currentUser = JSON.parse(localStorage.getItem("user"));
+
+  return (
+    <figure className="figure py-2 mx-2 rounded display-flex">
+      <div className="figure-img img-fluid rounded" alt="..."
+        style={{
+          backgroundImage: `url(${photo.imagePath})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "250px",
+          width: "100%"
+        }}></div>
+      <figcaption className="figure-caption">{photo.title}</figcaption>
+      <p className="figure-caption">{photo.description}</p>
+
+      {currentUser && photo.userID === currentUser.id && (
+        <button
+          onClick={() => onDelete(photo.id)}
+          className="btn btn-dark mt-2">
+          Delete
+        </button>
+      )}
+    </figure>
+  );
+}
 
 export default DashboardPhoto
