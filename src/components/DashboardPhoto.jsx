e7071d298f81f83b02a8c06244a41bd96e1db98a
Alejandro Vaquera Lopez <alejandrovaqueralopez@gmail.com>
2026-04-20 22:49:05 -0700

    feat:

--- src/components/DashboardPhoto.jsx
+++ src/components/DashboardPhoto.jsx
@@ -6,27 +6,65 @@ function DashboardPhoto({ photo, onDelete }) {
 
   const currentUser = JSON.parse(localStorage.getItem("user"));
     return (
-        <figure className="figure py-2 mx-2 rounded d-flex">
-          <div className="figure-img img-fluid rounded" alt="..." 
-            style={{
-              backgroundImage: `url(${photo.imagePath})`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              height: "250px",
-              width: "100%"
-            }}></div>
-          <div className="figure-content d-flex justify-content-center align-items-center">
-            <div className="figure-sub-content">
-              <figcaption className="figure-caption">{photo.title}</figcaption>
-              <p className="figure-caption">{photo.description}</p>
+        <figure className="figure py-2 mx-2 rounded ">
+          <div d-flex>
+            <div className="figure-img img-fluid rounded" alt="..." 
+              style={{
+                backgroundImage: `url(${photo.imagePath})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                height: "250px",
+                width: "100%"
+              }}></div>
+            <div className="figure-content d-flex justify-content-start align-items-center">
+              <div className="figure-sub-content">
+                <figcaption className="figure-caption">{photo.title}</figcaption>
+                <p className="figure-caption">{photo.description}</p>
+              </div>
+              {currentUser && photo.userID === currentUser.id && (
+                <button
+                  onClick={() => onDelete(photo.id)}
+                  className="btn btn-dark mt-2">
+                  Delete
+                </button>
+              )}
             </div>
-            {currentUser && photo.userID === currentUser.id && (
-              <button
-                onClick={() => onDelete(photo.id)}
-                className="btn btn-dark mt-2">
-                Delete
-              </button>
-            )}
+          </div>
+          <div >
+            <div class="accordion accordion-flush" id="accordionFlushExample">
+
+              <div class="accordion-item">
+                <h2 class="accordion-header">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                    Accordion Item #1
+                  </button>
+                </h2>
+                <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                  <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item’s accordion body.</div>
+                </div>
+              </div>
+              <div class="accordion-item">
+                <h2 class="accordion-header">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                    Accordion Item #2
+                  </button>
+                </h2>
+                <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                  <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item’s accordion body. Let’s imagine this being filled with some actual content.</div>
+                </div>
+              </div>
+              <div class="accordion-item">
+                <h2 class="accordion-header">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                    Accordion Item #3
+                  </button>
+                </h2>
+                <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                  <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item’s accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                </div>
+              </div>
+            </div>
+
           </div>
         </figure>
 
