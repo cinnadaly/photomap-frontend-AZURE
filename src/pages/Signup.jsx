229e60b99a6b692414bfe081ad20d832eb373656
Alejandro Vaquera Lopez <alejandrovaqueralopez@gmail.com>
2026-04-22 17:20:48 -0700

    feat:

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -14,6 +14,7 @@ function Signup({ setIsAuthenticated, setUser }) {
             headers: {
                 'Content-Type': 'application/json',
             },
+            credentials: "include",
             body: JSON.stringify(data)
         });
 
